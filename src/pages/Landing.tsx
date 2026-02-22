9fd450bd405ed0ee9c76ba7f63e449edf84b66dd
Agbeleshe <danielagbeleshe@gmail.com>
2026-02-22 16:12:06 +0100

    feat:implimented the connect wallect modal

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../components/Landing/Hero';
+import LandingHeader from '../components/Landing/LandingHeader';
 
 const Landing: React.FC = () => {
     return (
-        <div style={{ backgroundColor: '#020b0d', minHeight: '100vh' }}>
+        <div style={{ backgroundColor: '#020b0d', minHeight: '100vh', position: 'relative' }}>
+            <LandingHeader />
             <Hero />
             {/* Future sections like features, testimonials, etc. can be added here */}
         </div>
